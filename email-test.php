ad4a64b4c7e0a433013e04621ba849b26e973f43
Eric King <eric@webdeveric.com>
2014-05-19 17:20:04 -0500

    Updated message class to use built in WP class names

--- email-test.php
+++ email-test.php
@@ -1,51 +1,63 @@
 <?php
 /*
 Plugin Name: Email Test
+Plugin Group: Utilities
+Plugin URI: http://phplug.in/
+Version: 0.2
 Description: This plugin sends a simple test message to the admin or an email address of your choice.
 Author: Eric King
-Version: 0.2
 Author URI: http://webdeveric.com/
 */
 
-function email_test_menu(){
-	$settings_page = add_options_page( __('Email Test'), __('Email Test'), 'manage_options', 'email-test', 'email_test_page' );
+
+/*
+@todo
+- add wp nonce field instead of action send-test.
+- update styles
+*/
+
+function email_test_menu()
+{
+    $settings_page = add_options_page('Email Test', 'Email Test', 'manage_options', 'email-test', 'email_test_page');
 }
 add_action( 'admin_menu', 'email_test_menu' );
 
 
-function email_test_page(){
+function email_test_page()
+{
+    $message     = '';
+    $admin_email = get_option('admin_email');
+    $email       = $admin_email;
 
-	$message = '';
+    if (isset($_POST['action']) && $_POST['action'] == 'send-test') {
 
-	$admin_email = get_option('admin_email');
-	$email = $admin_email;
+        if (filter_has_var(INPUT_POST, 'email'))
+            $email = $_POST['email'];
 
-	if( isset( $_POST['action'] ) && $_POST['action'] == 'send-test' ){
+        $valid_email = filter_var($email, FILTER_VALIDATE_EMAIL);
 
-		if( filter_has_var( INPUT_POST, 'email' ) )
-			$email = $_POST['email'];
+        if ($valid_email === false) {
 
-		$valid_email = filter_var( $email, FILTER_VALIDATE_EMAIL );
+            $message = sprintf('<div class="error"><p>Invalid email address: %s</p></div>', $email);
 
-		if( $valid_email === false ){
-			$message = sprintf('<pre class="message">Invalid email address: %s</pre>', $email );
-		} else {
-			$ok = wp_mail( $valid_email, $_SERVER['HTTP_HOST'] . ' - Server Email Test', 'this is only a test.');
-			$message = sprintf('<pre class="message">Email sent = %s</pre>', print_r( $ok, true ) );
-		}
+        } else {
 
-	}
-?>
+            $ok = wp_mail( $valid_email, 'wp_mail test from ' . $_SERVER['HTTP_HOST'], 'this is only a test.');
+            $message = sprintf('<div class="updated"><p>Email sent = %s</p></div>', print_r($ok, true));
 
-<h1>Email Test</h1>
+        }
+    }
+    ?>
 
-<?php echo $message; ?>
+    <h1>Email Test</h1>
 
-<form action="" method="post">
-	<input type="hidden" name="action" value="send-test" />
-	<input type="email" name="email" id="email" placeholder="email address" value="<?php echo $email; ?>" style="width:200px;" />
-	<button type="submit" class="button-primary">Send Test Email</button>
-</form>
+    <?php echo $message; ?>
 
-<?php
+    <form action="" method="post">
+        <input type="hidden" name="action" value="send-test" />
+        <input type="email" name="email" id="email" placeholder="email address" required value="<?php echo $email; ?>" />
+        <button type="submit" class="button-primary">Send Test Email</button>
+    </form>
+
+    <?php
 }
